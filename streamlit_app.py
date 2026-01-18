fb2414d01d697d6cc7d34e9145ed7b343a35558d
CamaraAmidou <camaraamidou94@gmail.com>
2026-01-18 01:05:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,11 @@ st.set_page_config(
     page_title='Ai project with Babucarr',
     page_icon=':earth_africa:',
 )
-st.write(raw_data)
+
 with st.expander("View data"):
-  df = pd.read_csv("cvd_synthetic_dataset_v0.2.csv")
+    st.write(raw_data)
+    df = pd.read_csv("cvd_synthetic_dataset_v0.2.csv")
+    
 
 
 
